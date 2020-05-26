a78e3cc44327413675caa5cad3293b59de26cd7c
annagaines <gaines.annab@gmail.com>
2020-05-26 10:25:17 -0400

    not sure

--- findPrimers.py
+++ findPrimers.py
@@ -44,14 +44,19 @@ def main(genomelist1,genomelist2, whiteList,blackList,percentIdentity, log, freq
 	filelist1 = [line.rstrip('\n') for line in genomelist1]
 	filelist2 = [line.rstrip('\n') for line in genomelist2]
 	kmer_frequency_1 = len(filelist1) * frequency 
-	
+	kmer_frequency_2 = 1- int(kmer_frequency_1)
+
 	group1_genome, group2_genome = get_genome_groups(filelist1, filelist2)
+	make_jellyfish_directories("g1")
 	with Pool(10) as pool:
 		pool.map(partial(get_kmers, group='g1'), filelist1)
 		pool.map(partial(get_kmers, group='g1'), filelist1)
-	# combine_freq_and_filt(kmer_frequency_1 )
-	# calc_tm(uniq_kmer_file)
-	# off_target_check(uniq_filtered_kmers,percentIdentity)
+	combine_freq_and_filt(kmer_frequency_1, 'g1')
+	combine_freq_and_filt(kmer_frequency_1, 'g2')
+	calc_tm("g1_uniq_kmers_freq_filt.fasta", 'g1')
+	calc_tm("g2_uniq_kmers_freq_filt.fasta", 'g2')
+	compare_kmers("g1_uniq_kmers_freq_bio_filt.fasta","g2_uniq_kmers_freq_bio_filt.fasta")
+	#off_target_check("g1_uniq_kmers_freq_bio_filt.fasta",percentIdentity)
 	# check_nt_blast_results(whiteList,blackList,nt_blast_results)
 
 def get_genome_groups(filelist1, filelist2):
@@ -68,29 +73,36 @@ def get_genome_groups(filelist1, filelist2):
 		group2_genome[genome] = "g2"
 	return	(group1_genome, group2_genome)
 
+def make_jellyfish_directories(group):
+	if os.path.isdir(f"kmers/{group}"):
+		shutil.rmtree(f"kmers/{group}")
+	if os.path.isfile(f"kmers/{group}"):
+		os.remove(f"kmers/{group}")
+	os.mkdir(f"kmers/{group}")
+
 #Take each file in filelist and break create .jf and then .kmer files
 def get_kmers(file, group):
-	file = file.rstrip()
+	file = file.rstrip()	
 	#Jellyfish commands to generate the kmers
 	get_kmer_count = f"jellyfish count -C -m 21 -s 100M -o kmers/{group}/{(file).replace('_genomic.fna','')}.jf assemblies/{file}"
 	get_kmers = f"jellyfish dump kmers/{group}/{(file).replace('_genomic.fna','')}.jf |grep -v '>' > kmers/{group}/{(file).replace('_genomic.fna','')}.kmers"
 	print(get_kmer_count)
-	#subprocess.check_output(get_kmer_count, shell=True)		
+	subprocess.check_output(get_kmer_count, shell=True)		
 	print(get_kmers)
-	#subprocess.check_output(get_kmers, shell=True)
+	subprocess.check_output(get_kmers, shell=True)
 
 #Combine seperate .kmer files and filter it based on the given frequency	
-def combine_freq_and_filt():
-	combine_kmers = "cat kmers/*.kmers |sort |uniq -c > combined_uniq_kmers.txt" 
-	freq_filt_kmers = " cat combined_uniq_kmers.txt |awk '$1 >= 3 {print  $2}'  |nl |awk '{print \">kmer_\"$1 \"\\n\"$2}' >uniq_kmers_freq_filt.fasta"
+def combine_freq_and_filt(frequency, group):
+	combine_kmers = f"cat kmers/{group}/*.kmers |sort |uniq -c > {group}_combined_uniq_kmers.txt" 
+	freq_filt_kmers = f"cat {group}_combined_uniq_kmers.txt |awk '$1 >= {frequency} {{print  $2}}'  |nl |awk '{{print \">kmer_\"$1 \"\\n\"$2}}' > {group}_uniq_kmers_freq_filt.fasta"
 	subprocess.check_output(combine_kmers, shell=True)
 	subprocess.call(freq_filt_kmers, shell=True)
 
-# uniq_kmer_file = open("uniq_kmers_freq_filt.fasta","r")
 
-def calc_tm(file):
+def calc_tm(file, group):
 	#Read the kmer fasta file in 2 lines at a time, generate dictionary that will save: kmer, kmer seq, qual, Tm, GC
-		for line in iter(lambda: list(islice(file,2)),()):
+	with open(file,'r') as in_file:
+		for line in iter(lambda: list(islice(in_file,2)),()):
 			line = [l.strip() for l in line]
 			if len(line) <2:
 				break
@@ -107,11 +119,13 @@ def calc_tm(file):
 				#This Tm equation was taken from https://primerdigital.com/fastpcr/m7.html with 2.75 added because it brings the Tm closest to the Tm reported by the tool used by RDB http://www.operon.com/tools/oligo-analysis-tool.aspx
 				temp = 69.3 +((41*(counts['G']+counts['C'])-650)/l) 
 				if 49 < float(temp) < 66:
-					with open("uniq_kmers_freq_bio_filt.fasta", 'w') as out_file:
+					with open(f"{group}_uniq_kmers_freq_bio_filt.fasta", 'w') as out_file:
 						out_file.write(f"{line[0]}\n{line[1]}")
 					out_file.close()
 
-# uniq_filtered_kmers = "uniq_kmers_freq_bio_filt.fasta"
+
+def compare_kmers(group1, group2):
+	combine_kmer_cmd = f"cat {group1} {group2}| sort --parallel 20 -S 10G| uniq -c | awk '$1 == 1 {{print $2}}' |n1 | awk '{{print \">kmer_\"$1\"\\n\" $2 }}' > diffKmers.fasta "
 
 def off_target_check(uniq_filtered_kmers, percent_identity):
 	'''Use the kmer file filtered by frequency, GC content, melting temperature, and homopolymers as blast query against nt database.
